c9d2a4d335d0300a067d83b56288a00e876be367
Reuven Etzion <retzion@gmail.com>
2016-01-24 00:53:29 -0700

    ui changes to mailchimp subscription form

--- public/assets/js/forms/mailchimp.js
+++ public/assets/js/forms/mailchimp.js
@@ -1,20 +1,26 @@
+var ajaxPostInProgress = false
+
+var formPostTimedOut = function() {
+  if(ajaxPostInProgress)
+    $("#formRequestBuild").html('<h2>Something went wrong. Please refresh the page and try again.</h2>')
+}
 
 $.fn.serializeObject = function()
 {
-    var o = {};
-    var a = this.serializeArray();
+    var o = {}
+    var a = this.serializeArray()
     $.each(a, function() {
         if (o[this.name] !== undefined) {
             if (!o[this.name].push) {
-                o[this.name] = [o[this.name]];
+                o[this.name] = [o[this.name]]
             }
-            o[this.name].push(this.value || '');
+            o[this.name].push(this.value || '')
         } else {
-            o[this.name] = this.value || '';
+            o[this.name] = this.value || ''
         }
-    });
-    return o;
-};
+    })
+    return o
+}
 
 
 
@@ -22,27 +28,32 @@ $("#formRequestBuildSubmit").on('click', function() {
   var formData = $('#formRequestBuild').serializeObject()
   if(formData.MERGE0 && formData.MERGE0 != '') {
     formData.call = 'getbrave'
+    ajaxPostInProgress = true
     $("#formRequestBuildSubmit").text('Sending...')
+    window.setTimeout(formPostTimedOut, 300);
     $.ajax({
        url: '/api/mailchimp',
        type: 'POST',
        dataType: 'json',
        data: formData,
        error: function(err) {console.log('err',err)
-            $("#formRequestBuild").html('<h2>'+err.responseText+'</h2>')
+          ajaxPostInProgress = false
+          $("#formRequestBuild").html('<h2>'+err.responseText+'</h2>')
        },
-       success: function(data) {console.log(data)
-        if(data.euid)
-        {
+       success: function(data) {
+          ajaxPostInProgress = false
+          if(data.euid)
+          {
             $("#formRequestBuild").html($('#formRequestBuildThankYou').html())
-        }
-        else
-        {
+          }
+          else
+          {
             console.log('failed',data)
             $("#formRequestBuild").html(data)
-        }
+          }
        }
     });
   }
 })
+
 
