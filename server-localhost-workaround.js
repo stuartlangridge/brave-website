9abd9024260862f1eb0be228cec3390184a2facd
Reuven Etzion <retzion@gmail.com>
2016-01-28 14:53:06 -0700

    HSTS workaround for localhost

---
+++ server-localhost-workaround.js
@@ -0,0 +1,129 @@
+'use strict'
+
+const Hapi = require('hapi')
+const fs = require('fs')
+const mailchimp = require('./mailchimp.js')
+var assets = require('./assets.js')
+
+const server = new Hapi.Server()
+
+// temporary workaround to allow localhost with HSTS
+var connections = [
+      // non-local host
+      {
+          port: process.env.PORT || 3000,
+      },
+      // localhost
+      {
+          host: 'localhost',
+          port: 3001,
+          tls: {
+            key: fs.readFileSync('./server.key'),     // Path to key
+            cert: fs.readFileSync('./server.crt'),      // Path to self-signed Certificate
+
+            // This is necessary only if using the client certificate authentication.
+            requestCert: true,
+
+            // This is necessary only if the client uses the self-signed certificate.
+            ca: []
+        }
+      }
+  ]
+
+// IMPORTANT : If you toggle Connections, toggle back to [0] if not localhost
+server.connection(connections[1])
+
+server.register({ register: require('crumb'), options: 
+  { 
+    cookieOptions: { 
+      clearInvalid: true,
+      isSecure: true
+   } 
+  }
+ }, (err) => {
+  if (err) {
+    console.log('Failed to load crumb.')
+  }
+  
+  /* API endpoints */
+
+  // mailchimp methods
+  server.route({
+      method: 'POST',
+      path: '/api/mailchimp',
+      config: {
+        state: {
+          parse: true,
+          failAction: 'log' 
+        },
+        security: {
+          hsts: {
+            maxAge: 31536000,
+            includeSubDomains: true,
+            preload: true
+          }
+          , xframe: true
+        }
+      },
+      handler: function (request, reply) {
+        mailchimp.api(request, reply)
+      }
+  })
+
+})
+
+server.register(require('inert'), (err) => {
+  if (err) {
+    console.log('Failed to load inert.')
+  }
+  
+  // A server redirect to our favorite band, Brave Combo.
+  server.route({
+    method: 'GET',
+    path: '/bo/{path*}',
+    handler: function (request, reply) {
+      reply.redirect('http://bravecombo.com/' + (request.params.path ? request.params.path : ''))
+    }
+  })
+
+  // Serves static files out of public/
+  server.route({
+    method: 'GET',
+    path: '/{path*}',
+    config: {
+      state: {
+        parse: true, 
+        failAction: 'log' 
+      }, 
+      security: {
+        hsts: {
+          maxAge: 31536000,
+          includeSubDomains: true,
+          preload: true
+        }, 
+        xframe: true
+      }
+    },
+    handler: {
+      directory: {
+        path: './public'
+      }
+    }
+  })
+
+})
+
+server.ext('onRequest', function (request, reply) {
+  let isLocalhost = ((request.headers.host.substring(0,request.headers.host.indexOf(':'))) == 'localhost')
+    if (request.headers['x-forwarded-proto'] !== 'https' && !isLocalhost) {
+      return reply()
+        .redirect('https://' + request.headers.host + request.url.path)
+        .code(301);
+    }
+    reply.continue()      
+})
+
+server.start(() => {
+    console.log('Brave server running at:', server.info.uri);
+})
+
