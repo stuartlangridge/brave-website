a4f1f08be2ae124f41d228723f0967787afe822b
Reuven Etzion <retzion@gmail.com>
2016-01-29 13:26:59 -0700

    Piwik - track slider signup views, fails, and successes

--- public/assets/js/forms/mailchimp.js
+++ public/assets/js/forms/mailchimp.js
@@ -1,6 +1,7 @@
 //TODO: add timeout handler and ui notification
 
 $("#formRequestBuildSubmit").on('click', function() {
+  _paq.push(['trackEvent', 'SliderSignUpSubmission', 'Initiated'])
   var formData = $('#formRequestBuild').serializeObject()
   if(formData.MERGE0 && formData.MERGE0 != '') {
     if(!validateEmail(formData.MERGE0))
@@ -25,19 +26,20 @@ $("#formRequestBuildSubmit").on('click', function() {
        dataType: 'json',
        data: formData,
        error: function(err) {console.log('err',err)
-          // ajaxPostInProgress = false
           $("#formRequestBuild").html('<h2>'+err.responseText+'</h2>')
+          _paq.push(['trackEvent', 'SliderSignUpSubmission', 'Failed'])
        },
        success: function(data) {
           // ajaxPostInProgress = false
           if(data.euid)
           {
             $("#formRequestBuild").html($('#formRequestBuildThankYou').html())
+            _paq.push(['trackEvent', 'SliderSignUpSubmission', 'Success'])
           }
           else
           {
-            console.log('failed',data)
             $("#formRequestBuild").html(data)
+            _paq.push(['trackEvent', 'SliderSignUpSubmission', 'Failed'])
           }
        }
     });
